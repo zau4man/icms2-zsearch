8c7d42c26e034ef0919f99e79888ef66ccee6253
zau4man <evg-bulakh@yandex.ru>
2025-04-18 16:47:51 +0500

    Версия 1.0.2
    отключение кеширования виджета

--- package/system/controllers/zsearch/widgets/zsearch/widget.php
+++ package/system/controllers/zsearch/widgets/zsearch/widget.php
@@ -2,6 +2,9 @@
 
 class widgetZsearchZsearch extends cmsWidget {
 
+    // запрещаем кеширование виджета
+    public $is_cacheable = false;
+
     public function run() {
 
         cmsCore::loadControllerLanguage('search');
